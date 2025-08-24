cf3802e96f70b40f315431eb7ccb0a8c88fcfea3
chenchao <xxx@163.com>
2025-08-24 23:26:29 +0800

    修改项目备注

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -31,11 +31,13 @@
 
 
     
-    // projectName: '新建襄阳至荆门高速铁路"强电"、"弱电"系统集成及相关工程XJQD标',
+    // projectName: '新建襄阳至荆门高速铁路"强电"、"弱电"系统集成及相关工程XJQD标',//曹贼
     // contractorCorpCode: '914201001795600902',
     // projectCode: 'ff80808191e087a201921c8a9ae05b71', // 项目编码 请求3.3获取
     // projectCompanyCode: 'ff80808191e087a201921c8a9af55b73', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff808081921d021e0192db6898082705', // 班组编码 请求3.6获取
+
+
 
 
     // projectName: '中国水利水电第六工程局有限公司引江补汉工程土建施工及金结机电安装3标项目经理部',
@@ -45,18 +47,24 @@
     // teamSysNo: 'ff80808190a1d2310191593a7c8659d1', // 班组编码 请求3.6获取
 
 
-    // projectName: '引江补汉工程土建施工及金结机电安装4标',
+
+
+    // projectName: '引江补汉工程土建施工及金结机电安装4标',//中铁十八局
     // contractorCorpCode: '9112000010306009X2',
     // projectCode: 'ff80808195029e100195b10da8785439', // 项目编码 请求3.3获取
     // projectCompanyCode: 'ff80808195029e100195b10db340543b', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff80808195029e100195d256c31a0d5e', // 班组编码 请求3.6获取
 
 
-    // projectName: '引江补汉工程土建施工及金结机电安装5标',
+
+
+    // projectName: '引江补汉工程土建施工及金结机电安装5标',//中铁隧道局
     // contractorCorpCode: '91410300171075680N',
     // projectCode: 'ff80808195029e100195ad281f2c519f', // 项目编码 请求3.3获取
     // projectCompanyCode: 'ff80808195029e100195ad281f4351a1', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff808081961dd09a0196f710d1a205e3', // 班组编码 请求3.6获取
+
+
 
 
     // projectName: '中铁十二局集团有限公司引江补汉工程土建施工及金结机电安装6标',
@@ -67,11 +75,14 @@
 
 
 
-    // projectName: '中国葛洲坝集团股份有限公司引江补汉工程土建施工及金结机电安装7标项目经理部',
+
+    // projectName: '中国葛洲坝集团股份有限公司引江补汉工程土建施工及金结机电安装7标项目经理部',//高雪珊
     // contractorCorpCode: '914200006155710107',
     // projectCode: 'ff808081921d021e0192b233845a0341', // 项目编码 请求3.3获取
     // projectCompanyCode: 'ff808081921d021e0192b23384790343', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff808081961dd09a019680d786ae4492', // 班组编码 请求3.6获取
+
+
 
 
     // projectName: '中国水利水电第十四工程局有限公司引江补汉工程8标项目经理部',
@@ -80,22 +91,14 @@
     // projectCompanyCode: 'ff8080818d158603018d45115e243fe2', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff8080819059c7df01907c6ce51e3029', // 班组编码 请求3.6获取
 
-    // projectName: '引江补汉工程土建施工及金结机电安装9标',
+
+
+
+    // projectName: '引江补汉工程土建施工及金结机电安装9标',//水利水电四局
     // contractorCorpCode: '9163000022658124XK',
     // projectCode: 'ff808081921d021e0192d0e002c81bba', // 项目编码 请求3.3获取
     // projectCompanyCode: 'ff808081921d021e0192d0e002dc1bbc', // 参建单位编码 请求3.5获取
     // teamSysNo: 'ff808081934ce4ce01936ccc5fca4f2b', // 班组编码 请求3.6获取
-
-
-
-    // projectName: '引江补汉工程土建施工及金结机电安装9标',
-    // contractorCorpCode: '9163000022658124XK',
-    // projectCode: 'ff808081921d021e0192d0e002c81bba', // 项目编码 请求3.3获取
-    // projectCompanyCode: 'ff808081921d021e0192d0e002dc1bbc', // 参建单位编码 请求3.5获取
-    // teamSysNo: 'ff808081934ce4ce01936ccc5fca4f2b', // 班组编码 请求3.6获取
-
-
-
 
 
 
@@ -171,6 +174,16 @@
 // projectCode: '0000000090a1d4d70190fd941a63455f', // 项目编码 请求3.3获取
 // projectCompanyCode: '0000000090a1d4d70190fd941aa34561', // 参建单位编码 请求3.5获取
 // teamSysNo: 'ff8080819371bd900193cdfd0af63fd1', // 班组编码 请求3.6获取
+
+
+
+
+
+
+
+
+
+
 
 
 
